4448f33e0a18801c8cf653a43a3902b6ea31461e
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-04 03:32:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,64 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  def initialize
+
+  end
+
+def self.create
+song = Song.new
+Song.all.include?(song)
+song.save
+song
+end
+
+
+def self.new_by_name(name)
+song = Song.new
+song.name = name                              #???
+song.save
+song
+end
+
+def self.create_by_name(name)
+  song = Song.new
+  song.name = name                              #???
+  song.save
+  song
+end
+def self.find_by_name(name = nil)
+  @@all.detect{|x| x.name == name}
+
+end
+def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+def self.alphabetical
+ @@all.sort_by{|x| x.name}
+end
+
+def self.new_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+        song.name = (name.split(" - ")[1].chomp(".mp3"))
+        song.artist_name = (name.split(" - ")[0])
+        @@all << song
+        song
+      end
+def self.destroy_all()
+  @@all.clear
+end
+
 
   def self.all
     @@all
@@ -9,5 +67,4 @@ class Song
   def save
     self.class.all << self
   end
-
 end
